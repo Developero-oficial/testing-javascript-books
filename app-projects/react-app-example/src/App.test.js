0e9f0386be0e48b78ee686e368cf88b3fff4fefe
juan-carlos-correa <juan.correa.herrera@gmail.com>
2022-10-01 16:52:27 -0500

    added interaction test

--- app-projects/react-app-example/src/App.test.js
+++ app-projects/react-app-example/src/App.test.js
@@ -1,8 +1,28 @@
-import {render, screen} from '@testing-library/react'
+import React from 'react'
+import {screen, render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import App from './App'
 
-test('renders learn react link', () => {
+test('calculator UI', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+
+  expect(screen.getByLabelText('Height (M)')).toBeInTheDocument()
+  expect(screen.getByLabelText('Weight (KG)')).toBeInTheDocument()
+  expect(screen.getByRole('button', {name: 'Send'})).toBeInTheDocument()
+})
+
+test('calculator Thinness result', () => {
+  render(<App />)
+
+  const heightInput = screen.getByLabelText('Height (M)')
+  const weightInput = screen.getByLabelText('Weight (KG)')
+  const submitBtn = screen.getByRole('button', {name: 'Send'})
+
+  userEvent.type(heightInput, '1.7')
+  userEvent.type(weightInput, '50')
+  userEvent.click(submitBtn)
+
+  expect(screen.getByText('Result: 17.301')).toBeInTheDocument()
+  expect(screen.getByText('Estimation: Thinness')).toBeInTheDocument()
 })
