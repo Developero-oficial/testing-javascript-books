a6ec55e3be0a5c4c837d858821f3e438928d7c7d
juan-carlos-correa <juan.correa.herrera@gmail.com>
2022-10-01 21:21:27 -0500

    refactor cypress tests

--- app-projects/react-app-example/cypress/e2e/bmi-calculator/bmi-calculator.cy.js
+++ app-projects/react-app-example/cypress/e2e/bmi-calculator/bmi-calculator.cy.js
@@ -1,21 +1,21 @@
 describe('bmi calculator', () => {
   it('calculate Thinness result', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
 
     // Busca el input con etiqueta "height (KG)" y escribe el valor "1.7".
-    cy.get('#height').type('1.7')
+    cy.findByLabelText('Height (M)').type('1.7')
 
     // Busca el input con etiqueta "Weight (M)" y escribe el valor "50".
-    cy.get('#weight').type('50')
+    cy.findByLabelText('Weight (KG)').type('50')
 
     // Busca el botón con etiqueta "send" y haz click.
-    cy.get('button').click()
+    cy.findByRole('button').click()
 
     // Valida que exista "Result: 17.301" en la página.
-    cy.get('h2').contains('Result: 17.301')
+    cy.findByText('Result: 17.301')
 
     // Valida que exista "Estimation: Thinness" en la página.
-    cy.get('p').contains('Estimation: Thinness')
+    cy.findByText('Estimation: Thinness')
   })
 })
 
